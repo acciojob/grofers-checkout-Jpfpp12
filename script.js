588b9188047ceef6224fc6c9818c93f74185c5f7
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-19 18:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-
 const getSum = () => {
-  let prices = document.getElementsByClassName("price"); 
+  let prices = document.querySelectorAll(".price");
   let sum = 0;
-  
-  for (let index = 0; index < prices.length; index++) {
-    sum += parseFloat(prices[index].innerText) || 0; 
+
+  prices.forEach(priceElement => {
+    sum += parseFloat(priceElement.innerText) || 0;
+  });
+
+  let table = document.querySelector("table");
+  let totalRow = document.getElementById("total-row");
+
+  if (totalRow) {
+    totalRow.remove();
   }
-  
-  alert("Total Price: " + sum); 
+
+  totalRow = document.createElement("tr");
+  totalRow.id = "total-row";
+
+  let totalCell = document.createElement("td");
+  totalCell.colSpan = 3;
+  totalCell.innerText = "Total Price: ";
+
+  let totalValue = document.createElement("span");
+  totalValue.id = "ans";
+  totalValue.innerText = sum.toFixed(2);
+
+  totalCell.appendChild(totalValue);
+  totalRow.appendChild(totalCell);
+  table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
